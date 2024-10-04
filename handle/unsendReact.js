49d72c454289e4a854a70fc0037a08b01b2ee640
Kenzo Hiroshi <glennlotertetabalde12@gmail.com>
2024-10-05 07:48:27 +0800

    Update unsendReact.js

--- handle/unsendReact.js
+++ handle/unsendReact.js
@@ -1,6 +1,6 @@
 const unsendReact = async (api, event) => {
   if (event.type === "message_reaction") {
-    if (event.senderID === api.getCurrentUserID() && event.reaction === "👍") {
+    if (event.senderID === api.getCurrentUserID() && event.reaction === "❤️") {
       return api.unsendMessage(event.messageID);
     }
   }
